ce3ca0450768c89e77ad5743ac6bcb1451ec30f3
pasdoue <arthurl-@hotmail.fr>
2014-02-08 12:20:11 +0100

    petites modif de syntaxe de la BDD

--- BDD/création_bdd.sql
+++ BDD/création_bdd.sql
@@ -1,3 +1,6 @@
+/* Il manque l'insertion de la date du prochain RDV entre le référent et l'utilisateur
+	Ainsi que le champ de validation de communiquer les résultats à la famille
+*/
 CREATE TABLE Personne(
 id_personne INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 nom VARCHAR(50),
@@ -10,6 +13,7 @@ id_referent INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 nom VARCHAR(50),
 prenom VARCHAR(50),
 mail VARCHAR(50),
+telephone CHAR(10),
 id_referent_personne INT NOT NULL,
 CONSTRAINT FK_REFERENT_PERSONNE FOREIGN KEY (id_referent_personne) REFERENCES Personne(id_personne)
 );
@@ -33,18 +37,18 @@ CONSTRAINT FK_FAMILLE_PERSONNE FOREIGN KEY (id_famille_personne) REFERENCES Pers
 
 CREATE TABLE Date_questions(
 id_date INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-jour_releve_questionnaire DATE
+date_reponse_questionnaire DATE
 );
 
 CREATE TABLE Questionnaire(
 id_questionnaire INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-question_oui_non_1 INT,
-question_oui_non_2 INT,
-question_oui_non_3 INT,
 question_qcm_1 INT,
 question_qcm_2 INT,
-question_qcm_3 INT,
-mail VARCHAR(50),
+question_ouverte_3 INT,
+question_oui_non_4 INT,
+question_qcm_5 INT,
+question_oui_non_6 INT,
+question_oui_non_7 INT,
 id_questionnaire_personne INT,
 id_questionnaire_date INT,
 CONSTRAINT FK_QUESTIONNAIRE_PERSONNE FOREIGN KEY (id_questionnaire_personne) REFERENCES Personne(id_personne),
--- BDD/création_procédures.sql
+++ BDD/création_procédures.sql
@@ -8,36 +8,35 @@ DELIMITER $$
  *				Ajout de tables standard
  *****************************************************/
 
-
 CREATE PROCEDURE ajout_personne(p_nom VARCHAR(50),p_prenom VARCHAR(50),p_mail VARCHAR(50))
 BEGIN 
-INSERT INTO Personne(nom_societe, mail, inscription) 
+INSERT INTO Personne(nom, prenom, mail) 
 VALUES (p_nom, p_prenom, p_mail);
 END$$
 
-CREATE PROCEDURE ajout_referent(p_nom VARCHAR(50),p_prenom VARCHAR(50),p_mail VARCHAR(50), pid_referent_personne INT)
+CREATE PROCEDURE ajout_referent(p_nom VARCHAR(50),p_prenom VARCHAR(50),p_mail VARCHAR(50),p_telephone CHAR(10), pid_referent_personne INT)
 BEGIN 
-INSERT INTO Referent(nom,prenom,mail,id_referent_personne)
-VALUES (p_nom,p_prenom,p_mail, pid_referent_personne);
+INSERT INTO Referent(nom, prenom, mail, telephone, id_referent_personne)
+VALUES (p_nom, p_prenom, p_mail, p_telephone, pid_referent_personne);
 END$$
 
 CREATE PROCEDURE ajout_patron(p_nom VARCHAR(50),p_prenom VARCHAR(50),p_mail VARCHAR(50), pid_patron_personne INT)
 BEGIN 
-INSERT INTO Patron(nom,prenom,mail,id_patron_personne)
-VALUES (p_nom,p_prenom,p_mail, pid_patron_personne);
+INSERT INTO Patron(nom, prenom, mail, id_patron_personne)
+VALUES (p_nom, p_prenom, p_mail, pid_patron_personne);
 END$$
 
 CREATE PROCEDURE ajout_famille(p_nom VARCHAR(50),p_mail VARCHAR(50), pid_famille_personne INT)
 BEGIN 
-INSERT INTO Famille(nom,mail,id_famille_personne)
-VALUES (p_nom,p_mail, pid_famille_personne);
+INSERT INTO Famille(nom, mail, id_famille_personne)
+VALUES (p_nom, p_mail, pid_famille_personne);
 END$$
 
 /*On ajoute une date seulement lors de lajout du questionnaire!!
 => pas besoin de créer une procédure dajout de date*/
 
-CREATE PROCEDURE ajout_questionnaire(p_question_oui_non_1 INT,p_question_oui_non_2 INT,p_question_oui_non_3 INT,
-p_question_qcm_1 INT,p_question_qcm_2 INT,p_question_qcm_3 INT,p_mail VARCHAR(50),
+CREATE PROCEDURE ajout_questionnaire(p_question_qcm_1 INT,p_question_qcm_2 INT,p_question_ouverte_3 INT,
+p_question_oui_non_4 INT,p_question_qcm_5 INT,p_question_oui_non_6 INT, p_question_oui_non_7 INT,
 p_id_personne INT)
 BEGIN
 DECLARE v_id_date_questions INT; /* permet de stocker l'autoincrement actuel de la table Date_questions*/
@@ -47,11 +46,12 @@ WHERE TABLE_SCHEMA = 'handi-report' AND TABLE_NAME = 'Date_questions';
 
 INSERT INTO Date_questions(jour_releve_questionnaire) VALUES (CURRENT_DATE);
 
-INSERT INTO Questionnaire(question_oui_non_1,question_oui_non_2,
-question_oui_non_3,question_qcm_1,question_qcm_2,question_qcm_3,
-mail,id_questionnaire_personne, id_questionnaire_date)
-VALUES (p_question_oui_non_1,p_question_oui_non_2,
-p_question_oui_non_3,p_question_qcm_1,p_question_qcm_2,p_question_qcm_3,p_mail,p_id_personne, v_id_date_questions);
+INSERT INTO Questionnaire(question_qcm_1, question_qcm_2, question_ouverte_3,
+question_oui_non_4, question_qcm_5, question_oui_non_6, question_oui_non_7,
+id_questionnaire_personne, id_questionnaire_date)
+VALUES (p_question_qcm_1, p_question_qcm_2, p_question_ouverte_3,
+p_question_oui_non_4, p_question_qcm_5, p_question_oui_non_6, p_question_oui_non_7,
+p_id_personne, v_id_date_questions);
 END$$
 
 
